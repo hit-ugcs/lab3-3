31039faabd7e13d259146c5c0b81e4a8ea2f9c7a
Haposola <haposola@gmail.com>
2014-10-15 19:27:55 +0800

    small changes

--- app/models/grade.rb
+++ app/models/grade.rb
@@ -1,7 +1,7 @@
 class Grade < ActiveRecord::Base
   attr_accessible :disc, :exam, :lab, :name, :nick, :num, :pro
-  validates  :pro,  numericality:  {less_than:  40}
-  validates  :exam,  numericality:  {less_than:  40}
-  validates  :lab,  numericality:  {less_than:  10}
-  validates  :disc,  numericality:  {less_than:  10}
+  validates  :pro,  numericality:  {less_than_or_equal_to:  40}, presence: true
+  validates  :exam,  numericality:  {less_than_or_equal_to:  40}, presence: true
+  validates  :lab,  numericality:  {less_than_or_equal_to:  10}, presence: true
+  validates  :disc,  numericality:  {less_than_or_equal_to:  10}, presence: true
 end
